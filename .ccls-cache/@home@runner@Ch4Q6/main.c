587142d7fd94f153b89c020a9dd8ca1c57bf4d79
Iain Taylor <iain.taylor.94@gmail.com>
2023-06-24 14:56:02 +0000

    finished problem

--- .ccls-cache/@home@runner@Ch4Q6/main.c
+++ .ccls-cache/@home@runner@Ch4Q6/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  double x = 2.55;
+  double result = 3 * x * x * x - 5 * x * x + 6;
+
+  printf ("The result is: %f\n", result);
+  
   return 0;
 }
